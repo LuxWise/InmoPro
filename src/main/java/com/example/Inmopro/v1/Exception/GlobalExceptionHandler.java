b46aa6507b85d351b7d02106761d8f9af16235f3
ccherrerab <ccherrera913@ucatolica.edu.co>
2024-11-04 20:10:14 -0500

    v1.0.0 Contruccion de monitor Service con response

---
+++ src/main/java/com/example/Inmopro/v1/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.Inmopro.v1.Exception;
+
+import com.example.Inmopro.v1.Controller.MonitorCon.Response;
+import com.example.Inmopro.v1.Service.MonitorSer.MonitorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+// Esta clase maneja todas las excepciones globalmente
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedAccessException.class)
+    public ResponseEntity<Response> handleUnauthorizedAccessException(UnauthorizedAccessException ex) {
+        Response response = Response.builder()
+                .message(ex.getMessage())
+                .data(null)
+                .success(false)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+}
